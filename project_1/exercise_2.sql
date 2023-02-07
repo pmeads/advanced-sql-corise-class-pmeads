04e667c719f686b27dd3fe68904c95f6b4fe8dcf
Phil Meads <philme@ucsc.edu>
2023-02-07 14:40:37 -0800

    finish work on exercise 2

--- project_1/exercise_2.sql
+++ project_1/exercise_2.sql
@@ -1,13 +1,11 @@
 /*
-
-    The challenge query from project 1 ended up being quite challenging!! 
-    I think I was able to provide an answer for the first part of the second 
-    question, but I am really stumped at the moment on how to get a good recipe, 
-    just one, for the first food pref. I want it to be a random sample rather 
-    than just returning the min or such for something like “eggs” and have not 
-    figured out how to do that just yet.  I haven't used any hints yet or asked 
-    the TA for help just yet. I was hoping to spend a little more time monday on it
-
+   approach: 
+   1) get valid customers using address matched against us_cities table
+   2) get the customers food preferences and limit to first 3 using row_Number() window function
+   3) pivot results to create a column for each food preference
+   4) use min to get recipe ingredients by flattening recipe.tag_list column (first tried flattening ingredients column but that didn't work)
+   5) join back to customer food preference results , use any_value to get once matching recipe
+   6) order results by email
 */
 
 with 
@@ -17,105 +15,142 @@ with
 us_cities as (
     
     select 
-        city_name,
-        state_abbr,
+        upper(trim(city_name)) as city_name,
+        upper(trim(state_abbr)) as state_abbr,
         lat,
         long    
     from vk_data.resources.us_cities
     
 ),
 
--- Determine which customer addresses are good by comparing them to 
--- our us_cities data. At the same time, we'll save the lat/long data for later
-valid_customers as (
+-- Determine which customer id are good by comparing addresses to 
+-- our us_cities data. 
+valid_customer_ids as (
 
-    select distinct 
-        c.customer_id, 
-        c.email,
-        c.first_name
-    
-    from vk_data.customers.customer_data as c
-    join vk_data.customers.customer_address as a
-        on c.customer_id = a.customer_id
+    select distinct
+        a.customer_id
+    from vk_data.customers.customer_address as a
     join us_cities as ci on (
-         ci.city_name = upper(a.customer_city) 
-         and ci.state_abbr = a.customer_state
+         ci.city_name = upper(trim(a.customer_city))
+         and ci.state_abbr = upper(trim(a.customer_state))
     )
+
+),
+
+-- get the rest of the customer data we were asked to return
+customers as (
+    
+     select 
+         c.customer_id,
+         c.first_name,
+         c.email
+     from vk_data.customers.customer_data as c
+     join valid_customer_ids as vc
+        on vc.customer_id = c.customer_id
     
 ),
 
+-- from the survey, get the first 3 food prefs per customer
 customer_food_preferences as (
-    
-    select 
-        *
+ 
+    select
+        customer_id,
+        food_preference,
+        rownum
     from (
-        
-        select 
+
+        select
             c.customer_id,
-            c.email,
-            c.first_name,
-            cs.tag_id,
-            replace(rt.tag_property,' ','') as food_preference,
+            trim(replace(rt.tag_property, '"', '')) as food_preference,   
             row_number() over (partition by c.customer_id order by rt.tag_property) as rownum
-        from valid_customers as c
-        join vk_data.customers.customer_survey as cs 
+        from valid_customer_ids as c
+        join vk_data.customers.customer_survey as cs
             on cs.customer_id = c.customer_id
         join vk_data.resources.recipe_tags as rt
             on cs.tag_id = rt.tag_id
         where cs.is_active = TRUE
         order by c.customer_id, food_preference
-        
-    
-    ) 
+
+    )
     where rownum <= 3
 
 ),
 
+-- rotate the results so the 3 food prefs are columns
+customer_food_preferences_pivot as (
+
+    select *
+    from customer_food_preferences
+    pivot ( min(food_preference) for rownum in (1,2,3) ) 
+           as pivot_values (
+               customer_id, 
+               food_preference1, 
+               food_preference2, 
+               food_preference3
+           )
+
+),
+
+-- it seems like this should be where we match ingredients but it is in the tag_list instead
+recipe_ingredients_does_not_work as (
+
+    select
+        recipe_id,
+        recipe_name,
+        trim(replace(flat_ingredients.value, '"', '')) as ingredient
+    from vk_data.chefs.recipe,
+    table(flatten(ingredients)) as flat_ingredients
+
+),
+
+-- get all ingredients per recipe
 recipe_ingredients as (
 
     select 
-        recipe_id, 
+        recipe_id,
         recipe_name,
-        ltrim(rtrim(flat_ingredients.value)) as ingredient
-    from vk_data.chefs.recipe,
-    table(flatten(ingredients)) as flat_ingredients
-    --limit 1
+        trim(replace(flat_tag.value, '"', '')) as ingredient
+    from vk_data.chefs.recipe
+    , table(flatten(tag_list)) as flat_tag
 
 ),
 
-t1 as (
-
-    select 
-        ingredient, 
-        count(distinct recipe_id) number_distinct_recipies
-    from recipe_ingredients
-    group by ingredient
-
-),
-
-test1 as (
+-- find a recipe for food_preference1
+customer_food_preferences_with_recipe as (
     
-    select 
-        cfp.customer_id,
-        cfp.email,
-        cfp.first_name,
-        cfp.food_preference,
-        cfp.rownum,
-        ri.recipe_name
-    from customer_food_preferences as cfp
-    left join recipe_ingredients as ri 
-        on (cfp.food_preference = ri.ingredient 
-            and cfp.rownum = 1         
-           )
-    order by cfp.customer_id, cfp.food_preference
-
+    select
+        cfpp.customer_id,
+        cfpp.food_preference1,
+        cfpp.food_preference2,
+        cfpp.food_preference3,
+        any_value(ri.recipe_name) as recipe_name  
+    from customer_food_preferences_pivot as cfpp
+    left outer join recipe_ingredients as ri
+        on cfpp.food_preference1 = ri.ingredient
+    group by 
+        cfpp.customer_id,
+        cfpp.food_preference1,
+        cfpp.food_preference2,
+        cfpp.food_preference3
 
 ),
 
+-- put it all together and sort
 final as (
     
-    select * from test1
-
+    select
+        c.customer_id,
+        c.first_name,
+        c.email,
+        cfpr.food_preference1,
+        cfpr.food_preference2,
+        cfpr.food_preference3,
+        cfpr.recipe_name     
+    from customers as c
+    join customer_food_preferences_with_recipe as cfpr
+        on cfpr.customer_id = c.customer_id
+    order by
+        c.email
     
 )
 
